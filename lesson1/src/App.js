af7a44345a2fa72d6b601364d7cd0699746c2aac
Debian <admin@ip-172-31-22-60.eu-central-1.compute.internal>
2023-12-16 17:56:00 +0000

    added omri to the title

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with ben
+          Learn React with ben and omri
         </a>
       </header>
     </div>
